112609f2783f4da7613072111f975917f76e07ab
Lajos Kossuth <L.Kossuth@topdesk.com>
2019-04-26 15:41:13 +0200

    update gateway to use localstorage

--- src/store/gateway.js
+++ src/store/gateway.js
@@ -5,9 +5,20 @@ export { Gateway };
 
 function Gateway({ get } = axios) {
 	return {
-		async getChuckNorrisJoke() {
-			const { data } = await get('http://api.icndb.com/jokes/random?escape=javascript');
-			return data.value.joke;
+		async loadData() {
+			return JSON.parse(localStorage.getItem('recordedItems')) || [];
+		},
+		async saveData(itemToSave) {
+			const currentItems = this.loadData();
+			currentItems.push(itemToSave);
+			localStorage.setItem('recordedItems', JSON.stringify(currentItems));
+			return JSON.parse(localStorage.getItem('recordedItems'));
+		},
+		async deleteItem(itemId) {
+			const currentItems = this.loadData();
+			const updatedItems = currentItems.filter(item => item.id !== itemId);
+			this.saveData(updatedItems);
+			return this.loadData();
 		}
 	};
 }
