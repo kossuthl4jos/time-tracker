ffa43970918f146c66b2cc8aabaa680569c28d6f
Lajos Kossuth <L.Kossuth@topdesk.com>
2019-08-30 13:33:58 +0200

    remove unnecesary import to reduce bundle

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
-import 'bulma/css/bulma.css';
-// import 'src/app.less';
-import 'buefy/dist/buefy.css';
 import './style/app.scss';
 
 import 'babel-polyfill';
 import Vue from 'vue';
 import App from './app.vue';
 import vueSetup from './vue-setup';
-import Buefy from 'buefy';
 
 const app = {
 	el: '#app',
 	render: h => h(App)
 };
-new Vue(Object.assign(app, vueSetup, Vue.use(Buefy)));
+new Vue(Object.assign(app, vueSetup));
 
 // eslint-disable-next-line no-console
 console.log('Page loaded. You will not see this message when using HMR and editing vue files.');
