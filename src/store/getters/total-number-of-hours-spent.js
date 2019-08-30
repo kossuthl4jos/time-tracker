839b10caee937a313db8df9bd572c47e07e4d80b
Lajos Kossuth <L.Kossuth@topdesk.com>
2019-08-30 12:33:20 +0200

    make sure hours containing letters will not be added

--- src/store/getters/total-number-of-hours-spent.js
+++ src/store/getters/total-number-of-hours-spent.js
@@ -1,7 +1,17 @@
 export function totalNumberOfHoursSpent({ listOfItemsToDisplay }) {
-	let totalHours = 0;
-	for (var i = 0; i < listOfItemsToDisplay.length; i++) {
-		totalHours += parseInt(listOfItemsToDisplay[i].hours);
-	}
-	return totalHours;
+	return listOfItemsToDisplay.length === 0 ?
+	0:
+	listOfItemsToDisplay
+			.map(item => item.hours)
+			.reduce((a, b) => numOr0(a) + numOr0(b), 0);
 }
+
+function numOr0(item) {
+	for (let i = 0; i < item.length; i++) {
+		if(isNaN(parseInt(item[i]))){
+			return 0
+		}
+	}
+	return parseInt(item)
+} 
+
