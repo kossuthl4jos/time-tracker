85f939f618b3871dffcbecd8f2bdb2b6d3815cc5
Lajos Kossuth <L.Kossuth@topdesk.com>
2019-08-30 15:22:16 +0200

    introduce css grids

--- src/components/the-counter.vue
+++ src/components/the-counter.vue
@@ -1,24 +1,18 @@
 <template>
-	<div class="section has-text-centered">
-		<div class="columns">
-			<div class="column is-two-fifths" />
-
-			<div class="column">
-				<div class="subtitle"> Days </div>
-				<IOdometer
-					:value="numberOfDays + 100"
-					:theme="selectedTheme"
-					class="iOdometer" />
-			</div>
-			<div class="column">
-				<div class="subtitle"> Hours </div>
-				<IOdometer
-					:value="numberOfHours + 100"
-					:theme="selectedTheme"
-					class="iOdometer" />
-			</div>
-
-			<div class="column is-two-fifths" />
+	<div class="section has-text-centered counter-grid-container">
+		<div class="subtitle day-title-item"> Days </div>
+		<div class="day-item">
+			<IOdometer
+				:value="numberOfDays + 1000"
+				:theme="selectedTheme"
+				class="iOdometer" />
+		</div>
+		<div class="subtitle hour-title-item"> Hours </div>
+		<div class="hour-item">
+			<IOdometer
+				:value="numberOfHours + 100"
+				:theme="selectedTheme"
+				class="iOdometer" />
 		</div>
 	</div>
 </template>
@@ -26,7 +20,6 @@
 <script type="text/babel">
 import IOdometer from 'vue-odometer';
 import 'odometer/themes/odometer-theme-train-station.css';
-import 'odometer/themes/odometer-theme-digital.css';
 import 'odometer/themes/odometer-theme-car.css';
 
 export default {
@@ -59,5 +52,31 @@ export default {
 	}
 	.odometer .odometer-inside .odometer-digit:first-child {
 		display: none;
+	}
+	.odometer .odometer-inside .odometer-formatting-mark {
+		display: none;
+	}
+
+	.counter-grid-container {
+		display: grid;
+		grid-template-columns: auto auto auto auto;
+		justify-content: center;
+		column-gap: 100px;
+	}
+	.day-title-item {
+		grid-column: 2;
+		grid-row: 1;
+	}
+	.hour-title-item {
+		grid-column: 3;
+		grid-row: 1;
+	}
+	.day-item {
+		grid-column: 2;
+		grid-row: 2;
+	}
+	.hour-item {
+		grid-column: 3;
+		grid-row: 2;
 	}
 </style>
