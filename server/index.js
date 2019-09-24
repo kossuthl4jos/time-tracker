a5b34837f1501c4e6aa2e1c87ffa590fd95f3d35
Lajos Kossuth <kossuthl4jos@gmail.com>
2019-09-24 09:22:02 +0200

    simplify server

--- server/index.js
+++ server/index.js
@@ -2,14 +2,13 @@ const path = require('path');
 const express = require('express');
 const fallback = require('express-history-api-fallback');
 const compression = require('compression');
-const setupApi = require('./setup-api');
 const app = express();
 
 const publicPath = process.env.PUBLIC_PATH || process.env.npm_package_config_default_public_path;
-console.log(`Serving content from public path /my-vue-webpack/`);
+console.log(`Serving content from public path ` + publicPath);
 const root = path.resolve(__dirname, '../dist');
 
-setupApi(app);
+// setupApi(app); --> check setup-api.js
 app.use((req, res, next) => {
   res.header('Cache-Control', 'max-age=1');
 	next();
--- server/setup-api.js
+++ server/setup-api.js
@@ -1,6 +1,7 @@
 const proxy = require('express-http-proxy');
 const proxyConfig = require('./proxy.config');
 
+//this could be used to proxy request to the app/api to somewhere else
 module.exports = function setupApi(app) {
 	app.use('/api', proxy(proxyConfig.apiHost, { preserveHostHdr: true }));
 };
